023a90eca96745530096ff83a782aad11129b7ed
moashour <ashour.mohammad@gmail.com>
2020-04-24 12:15:27 +0000

    Refactor: removing Resources class library project and adding resource files directly in main web project

--- Heaventure/Resources.Designer.cs
+++ Heaventure/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Resources {
+namespace Heaventure.CultureResources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Heaventure.CultureResources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
